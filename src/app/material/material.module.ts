b409db2f5386632aa8a909b84449ea2b538bd53f
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-11 13:04:08 -0500

    add list item module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,8 +3,15 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  exports: [MatButtonModule, MatSidenavModule, MatToolbarModule, MatIconModule],
+  exports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+  ],
 })
 export class MaterialModule {}
