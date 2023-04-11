068a860e74afaf00d7c555294367a8ba3c80b7bc
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-11 00:18:24 -0500

    add material modules sidenav

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [MatButtonModule, MatSidenavModule, MatToolbarModule, MatIconModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
