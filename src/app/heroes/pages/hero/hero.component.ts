f82335372df31385121a2bb293dda9933c1b94cc
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-13 23:30:29 -0500

    add hero component view

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 // rxjs
 import { switchMap, tap } from 'rxjs';
 // service
@@ -10,21 +10,38 @@ import { Hero } from '../../interfaces/hero.interface';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
+  styles: [
+    `
+      img {
+        width: 100%;
+        border-radius: 5px;
+      }
+
+      h1 {
+        margin-bottom: 0px;
+      }
+    `,
+  ],
 })
 export class HeroComponent implements OnInit {
   hero!: Hero;
 
   constructor(
-    private route: ActivatedRoute,
-    private serviceHero: HeroesService
+    private activatedRoute: ActivatedRoute,
+    private serviceHero: HeroesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.route.params
+    this.activatedRoute.params
       .pipe(
         switchMap(({ id }) => this.serviceHero.getHeroById(id)),
         tap(console.log)
       )
       .subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack() {
+    this.router.navigate(['hero/list']);
   }
 }
