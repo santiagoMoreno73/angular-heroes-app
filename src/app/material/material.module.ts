761672dbb10f0170dd3f84142cb1a8853c3205a6
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-14 14:29:11 -0500

    add MatAutocompleteModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,17 +7,23 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   exports: [
+    MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
-    MatSidenavModule,
-    MatToolbarModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSidenavModule,
+    MatToolbarModule,
     MatGridListModule,
+    MatFormFieldModule,
   ],
 })
 export class MaterialModule {}
