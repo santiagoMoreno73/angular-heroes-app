5fa99aa37e46374d24598a08b038d4fcdfa3175b
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-14 14:29:19 -0500

    add search component

--- src/app/heroes/pages/search/search.component.ts
+++ src/app/heroes/pages/search/search.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
+  term: string = '';
+  heroes: Hero[] = [];
+  heroSelected: Hero | undefined;
 
+  constructor(private heroesService: HeroesService) {}
+
+  search() {
+    this.heroesService
+      .getSuggestions(this.term.trim())
+      .subscribe((data) => (this.heroes = data));
+  }
+
+  optionSelected(event: MatAutocompleteSelectedEvent) {
+    if (!event.option.value) {
+      this.heroSelected = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.term = hero.superhero;
+
+    this.heroesService
+      .getHeroById(hero.id!)
+      .subscribe((hero) => (this.heroSelected = hero));
+  }
 }
