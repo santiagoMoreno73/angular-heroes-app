cb3c84ec691ee299cc04d225dc6c00740fd011ef
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-13 23:37:33 -0500

    add base url

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,18 +3,20 @@ import { HttpClient } from '@angular/common/http';
 // interfaces
 import { Hero } from '../interfaces/hero.interface';
 import { Observable } from 'rxjs';
+import { environment } from 'src/app/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
+  private baseUrl: string = environment.baseUrl;
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>('http://localhost:3000/heroes');
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroById(id_hero: string): Observable<Hero> {
-    return this.http.get<Hero>(`http://localhost:3000/heroes/${id_hero} `);
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id_hero} `);
   }
 }
