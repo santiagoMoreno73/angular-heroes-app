7f767ad9b4bb22dc768950b0a78914ee65e184a6
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-13 23:14:43 -0500

    add list component

--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  heroes: Hero[] = [];
 
+  constructor(private heroesServices: HeroesService) {}
+
+  ngOnInit(): void {
+    this.heroesServices
+      .getHeroes()
+      .subscribe((resp) => (this.heroes = [...resp]));
+  }
 }
