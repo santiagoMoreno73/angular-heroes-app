40d77031179b51b15c37b7c2acd971d2b6cd982f
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-13 23:14:20 -0500

    add progress grid & list modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
   ],
 })
 export class MaterialModule {}
