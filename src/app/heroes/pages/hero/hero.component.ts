8df82b7e3b55ecba7e6195d736a766c02effbcc2
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-13 23:14:29 -0500

    add hero component

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// rxjs
+import { switchMap, tap } from 'rxjs';
+// service
+import { HeroesService } from '../../services/heroes.service';
+// interface
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+  hero!: Hero;
 
+  constructor(
+    private route: ActivatedRoute,
+    private serviceHero: HeroesService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap(({ id }) => this.serviceHero.getHeroById(id)),
+        tap(console.log)
+      )
+      .subscribe((hero) => (this.hero = hero));
+  }
 }
