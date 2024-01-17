eb7caa79093585ce2e36479768e2dc6186ed8711
santiago.moreno@geoaitech.com <santiago.moreno@geoaitech.com>
2024-01-16 19:53:16 -0500

    added login function

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
 })
 export class LoginPageComponent {
+  constructor(public router: Router, private authService: AuthService,) { }
 
+  onLogin(): void {
+    this.authService.login("santimor2009@hotmail.com", "2323")
+      .subscribe(res => {
+        if (res) this.router.navigate(['/']);
+      });
+  }
 }
